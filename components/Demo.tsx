f3896a8c5af22f1ae4c5462fb80329e4281c6098
Mkhotamirais <mkhotamirais@gmail.com>
2024-06-24 04:43:49 +0700

    aceternityui khotami

---
+++ components/Demo.tsx
@@ -0,0 +1,13 @@
+import { AnimatedPinDemo } from "./demo/AnimatedPinDemo";
+import { AuroraBackgroundDemo } from "./demo/AuroraBackgroundDemo";
+import { BackgroundBeamsDemo } from "./demo/BackgroundBeamsDemo";
+
+export default function Demo() {
+  return (
+    <div>
+      <AnimatedPinDemo />
+      <AuroraBackgroundDemo />
+      <BackgroundBeamsDemo />
+    </div>
+  );
+}
