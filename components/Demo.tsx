ffa9a76f7d5cc03f37ecc27a317ea9f776e0cdd4
Mkhotamirais <mkhotamirais@gmail.com>
2024-06-24 05:08:51 +0700

    masih copas

--- components/Demo.tsx
+++ components/Demo.tsx
@@ -1,13 +1,21 @@
 import { AnimatedPinDemo } from "./demo/AnimatedPinDemo";
 import { AuroraBackgroundDemo } from "./demo/AuroraBackgroundDemo";
 import { BackgroundBeamsDemo } from "./demo/BackgroundBeamsDemo";
+import { BackgroundBoxesDemo } from "./demo/BackgroundBoxesDemo";
+import { CardHoverEffectDemo } from "./demo/CardHoverEffectDemo";
+import { EvervaultCardDemo } from "./demo/EvervaultCardDemo";
+import { FloatingNavDemo } from "./demo/FloatingNavDemo";
 
 export default function Demo() {
   return (
     <div>
-      <AnimatedPinDemo />
+      <FloatingNavDemo />
+      <EvervaultCardDemo />
+      <CardHoverEffectDemo />
+      <BackgroundBoxesDemo />
       <AuroraBackgroundDemo />
       <BackgroundBeamsDemo />
+      <AnimatedPinDemo />
     </div>
   );
 }
